1a145393b5b7775bd9e288337fa9358ad54a5993
2KRN4U <2krn4u@gmail.com>
2020-05-30 21:10:42 -0700

    now supports any amt of players
    can also specify how long before swap

--- DeathSwap/src/com/tsukrn/DeathSwap/Main.java
+++ DeathSwap/src/com/tsukrn/DeathSwap/Main.java
@@ -1,16 +1,26 @@
 package com.tsukrn.DeathSwap;
 
 
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+//import org.bukkit.craftbukkit.libs.org.apache.commons.lang3.ArrayUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.inventory.ItemStack;
+//import java.util.Arrays;
+//import java.util.stream.IntStream;
+
+// Function to remove the element 
 
 
 public class Main extends JavaPlugin {
+	
 	
 	@Override
 	public void onEnable() {
@@ -22,36 +32,102 @@ public class Main extends JavaPlugin {
 		//shutdown
 	}
 	int i = 0; //keep track of time
+	int newLength = 0;
+	String[] playerArgs = new String[0];
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (label.equalsIgnoreCase("deathswap")) {
-			if (args.length <= 1 || args.length > 2) {
-				Player player = (Player) sender;
-				player.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "Enter two players.");
+			if (args.length > 2) {
+				String[] arguments = new String[(args.length-1)];
+				playerArgs = arguments.clone();
+				Player[] water = new Player [(args.length-1)];
+				Player[] amtOfPlayers = new Player[(args.length-1)];
+				Location[] playerLocations = new Location[(args.length-1)];
+				for (int x = 0; x < (args.length - 1); x++) {
+					playerArgs[x] = args[x];
+					water[x] = Bukkit.getPlayer(playerArgs[x]);
+					water[x].getInventory().clear();
+					water[x].getInventory().addItem(new ItemStack(Material.WATER_BUCKET));
+					//Bukkit.broadcastMessage("Gave item to " + Bukkit.getPlayer(arguments[x]) + "" + x);
+
+				}
+				for (World w:Bukkit.getWorlds()) {
+					w.setTime(0);
+					w.setStorm(false);
+				}
 				
-				return true;
-			}
-			
-			if (args.length == 2) {
-				String argument1 = args[0];
+				/*String argument1 = args[0];
 				String argument2 = args[1];
-	
+				String argument3 = args[2];
+				String argument4 = args[3];
+				Player water1 = Bukkit.getPlayer(argument1);
+				Player water2 = Bukkit.getPlayer(argument2);
+				Player water3 = Bukkit.getPlayer(argument3);
+				Player water4 = Bukkit.getPlayer(argument4);
+				water1.getInventory().addItem(new ItemStack(Material.WATER_BUCKET));
+				water2.getInventory().addItem(new ItemStack(Material.WATER_BUCKET));
+				water3.getInventory().addItem(new ItemStack(Material.WATER_BUCKET));
+				water4.getInventory().addItem(new ItemStack(Material.WATER_BUCKET));*/
+				
+				Bukkit.broadcastMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "***DeathSwap***\nCreated by: \u30C4KRN");
+				int time = Integer.parseInt(args[(args.length-1)]);
 				Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
 					@Override
 					public void run() {
 						if (i == 1)
-							Bukkit.broadcastMessage(ChatColor.RED + "" + ChatColor.BOLD + "Teleporting in 2 mins");
-						if(i > 110 && i < 120) {
-							Bukkit.broadcastMessage(ChatColor.RED + "" + ChatColor.BOLD + "Teleporting in " + (120 - i));
+							Bukkit.broadcastMessage(ChatColor.RED + "" + ChatColor.BOLD + "Teleporting in " + time + " secs");
+						if(i > (time - 10) && i < time) {
+							Bukkit.broadcastMessage(ChatColor.RED + "" + ChatColor.BOLD + "Teleporting in " + (time - i));
 						}
-						if (i == 120) {
-							Player player1 = Bukkit.getPlayer(argument1);
+						if (i == time) {
+							/*Player player1 = Bukkit.getPlayer(argument1);
 							Player player2 = Bukkit.getPlayer(argument2);
 							Location play1 = player1.getLocation();
 							Location play2 = player2.getLocation();
 							player1.teleport(play2);
-							player2.teleport(play1);
+							player2.teleport(play1);*/
+							
+							for (int x = 0; x < playerArgs.length;x++) {
+								//Bukkit.broadcastMessage("" + x + " " + i);
+								amtOfPlayers[x] = Bukkit.getPlayer(playerArgs[x]);
+								if (x != (playerArgs.length-1)) {
+									amtOfPlayers[x+1] = Bukkit.getPlayer(playerArgs[x+1]);
+									playerLocations[x+1] = amtOfPlayers[x+1].getLocation();
+								}
+								playerLocations[x] = amtOfPlayers[x].getLocation();
+								if (x == playerArgs.length-1) {
+									amtOfPlayers[x].teleport(playerLocations[0]);
+								} else {
+									//Bukkit.broadcastMessage("" + amtOfPlayers[x+1].getName() + " - " + playerLocations[x+1]);
+									amtOfPlayers[x].teleport(playerLocations[(x+1)]);
+									//Bukkit.broadcastMessage("reeeeee is this working");
+								}
+							}
 							i = 0;
+						}
+						for (int a = 0; a < playerArgs.length; a++) {
+							Player[] playerAlive = new Player[playerArgs.length]; 
+							playerAlive[a] = Bukkit.getPlayer(playerArgs[a]);
+							if (playerAlive[a].isDead()) {
+								String deadPlayer = playerAlive[a].getName();
+								newLength = playerArgs.length;
+								for (int b = 0; b < playerArgs.length; b++) {
+									if(playerArgs[b].contains(deadPlayer)) { //https://stackoverflow.com/questions/47068605/java-remove-an-item-from-existing-string-array
+										newLength--;
+										//Bukkit.broadcastMessage("new length working?");
+									}
+								}
+								String[] results = new String[newLength];
+								int count = 0;
+								for (int b = 0; b < playerArgs.length; b++) {
+									if(!playerArgs[b].contains(deadPlayer)) {
+										results[count] = playerArgs[b];
+										count++;
+										//Bukkit.broadcastMessage("count working?");
+									}
+								}
+								playerArgs = results.clone();
+							}
 						}
 						i++;
 						}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DeathSwap
 my first minecraft mod xd
+USAGE: /deathswap Name Name time(in secs)
+EXAMPLE: /deathswap Player1 Player2 Player3 Player4 300
